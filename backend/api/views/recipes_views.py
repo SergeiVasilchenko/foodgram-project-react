5bcd647087193a1680c4678510d3cb4e816fe292
Sergei Vasilchenko <8045274@gmail.com>
2023-07-07 23:47:36 +0400

    CICD9.9.9.9.9.9.3

--- backend/api/views/recipes_views.py
+++ backend/api/views/recipes_views.py
@@ -1,23 +1,24 @@
 import django.contrib.auth
 import django.db.models
-# import django.http
-# import django_filters.rest_framework
 import recipes.models
-import rest_framework
-# import serializers.recipes_serializers
 from api.filters import IngredientFilter, RecipeFilter
 from api.pagination import CustomPagination
 from api.permissions import IsAdminOrReadOnly, IsAuthorOrReadOnly
+from django.core.exceptions import PermissionDenied
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import status, viewsets
+from rest_framework.decorators import action
+from rest_framework.permissions import SAFE_METHODS, IsAuthenticated
+from rest_framework.response import Response
 
 from ..serializers import recipes_serializers
 
 User = django.contrib.auth.get_user_model()
 
 
-class IngredientViewSet(rest_framework.ReadOnlyModelViewSet):
+class IngredientViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = recipes.Ingredient.objects.all()
     serializer_class = recipes_serializers.IngredientSerializer
     permission_classes = (IsAdminOrReadOnly,)
@@ -27,13 +28,13 @@ class IngredientViewSet(rest_framework.ReadOnlyModelViewSet):
     filterset_class = IngredientFilter
 
 
-class TagViewSet(rest_framework.ReadOnlyModelViewSet):
+class TagViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = recipes.Tag.objects.all()
     serializer_class = recipes_serializers.TagSerializer
     permission_classes = (IsAdminOrReadOnly,)
 
 
-class RecipeViewSet(rest_framework.ModelViewSet):
+class RecipeViewSet(viewsets.ModelViewSet):
     queryset = recipes.Recipe.objects.all()
     permission_classes = (IsAuthorOrReadOnly | IsAdminOrReadOnly,)
     pagination_class = CustomPagination
@@ -43,7 +44,7 @@ class RecipeViewSet(rest_framework.ModelViewSet):
     filterset_class = RecipeFilter
 
     def get_serializer_class(self):
-        if self.request.method in rest_framework.SAFE_METHODS:
+        if self.request.method in SAFE_METHODS:
             return recipes_serializers.RecipeReadSerializer
         return recipes_serializers.RecipeWriteSerializer
 
@@ -53,57 +54,57 @@ class RecipeViewSet(rest_framework.ModelViewSet):
 
     def add_item(self, model, user, pk):
         if model.objects.filter(user=user, recipe__id=pk).exists():
-            return rest_framework.Response(
-                status=rest_framework.status.HTTP_400_BAD_REQUEST
+            return Response(
+                status=status.HTTP_400_BAD_REQUEST
             )
         recipe = get_object_or_404(recipes.Recipe, id=pk)
         model.objects.create(user=user, recipe=recipe)
         serializer = recipes_serializers.RecipePreviewSerializer(recipe)
-        return rest_framework.Response(
+        return Response(
             serializer.data,
-            status=rest_framework.status.HTTP_201_CREATED
+            status=status.HTTP_201_CREATED
         )
 
     def delete_item(self, model, user, pk):
         item = model.objects.filter(user=user, recipe__id=pk)
         if item.exists():
             item.delete()
-            return rest_framework.Response(
-                status=rest_framework.status.HTTP_204_NO_CONTENT
+            return Response(
+                status=status.HTTP_204_NO_CONTENT
             )
-        raise rest_framework.PermissionDenied(
+        raise PermissionDenied(
             'У вас недостаточно прав редактировать данный рецепт'
         )
 
-    @rest_framework.action(
+    @action(
         detail=True,
         methods=['post', 'delete'],
-        permission_classes=[rest_framework.IsAuthenticated]
+        permission_classes=[IsAuthenticated]
     )
     def favorite(self, request, pk):
         if request.method == 'POST':
             return self.write_item(recipes.Favorites, request.user, pk)
         return self.delete_item(recipes.Favorites, request.user, pk)
 
-    @rest_framework.action(
+    @action(
         detail=True,
         methods=['post', 'delete'],
-        permission_classes=[rest_framework.IsAuthenticated]
+        permission_classes=[IsAuthenticated]
     )
     def shopping_cart(self, request, pk):
         if request.method == 'POST':
             return self.write_item(recipes.UsersRecipes, request.user, pk)
         return self.delete_item(recipes.UsersRecipes, request.user, pk)
 
-    @rest_framework.action(
+    @action(
         detail=False,
-        permission_classes=[rest_framework.IsAuthenticated]
+        permission_classes=[IsAuthenticated]
     )
     def download_shopping_cart(self, request):
         user = request.user
         if not user.shopping_cart.exists():
-            return rest_framework.Response(
-                status=rest_framework.status.HTTP_400_BAD_REQUEST
+            return Response(
+                status=status.HTTP_400_BAD_REQUEST
             )
         ingredients = recipes.RecipeIngredient.objects.filter(
             recipe__shopping_cart__user=request.user
