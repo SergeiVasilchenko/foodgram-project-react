5c58cfe09dbc65556fa10ea844ca693e69d0947e
Sergei Vasilchenko <8045274@gmail.com>
2023-07-08 04:08:17 +0400

    CICD9.9.9.9.9.9.9.3

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -5,7 +5,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.getenv('SECRET_KEY', default='secret_key')
 
