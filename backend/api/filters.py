da0ca74463764f2291ac5661c90d4137df16875a
Sergei Vasilchenko <8045274@gmail.com>
2023-07-11 01:38:00 +0400

    CICD9.9.9.9.9.9.9.9.9.9.4

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -23,3 +23,8 @@ class RecipeFilter(FilterSet):
     is_favorited = filters.BooleanFilter(method='filter_is_favorited')
     is_in_shopping_cart = filters.BooleanFilter(
         method='filter_is_in_shopping_cart')
+
+    def filter_is_favorited(self, queryset, name, value):
+        if value and not self.request.user.is_anonymous:
+            return queryset.filter(favorite__user=self.request.user)
+        return queryset
