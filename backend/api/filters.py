086a2a77c05fe9b064f0c345f2a50218d7b70e65
Sergei Vasilchenko <8045274@gmail.com>
2023-07-11 02:56:52 +0400

    CICD9.9.9.9.9.9.9.9.9.9.5

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -26,5 +26,5 @@ class RecipeFilter(FilterSet):
 
     def filter_is_favorited(self, queryset, name, value):
         if value and not self.request.user.is_anonymous:
-            return queryset.filter(favorite__user=self.request.user)
+            return queryset.filter(favorites__user=self.request.user)
         return queryset
